33da9cbe082a5e2a7bff45b51fd77426eb29cb65
omega1007 <41986987+omega1007@users.noreply.github.com>
2021-06-02 11:04:53 +0800

    Create profile.js

---
+++ profile.js
@@ -0,0 +1,12 @@
+export var firstName = "june"
+export var lastName = "Xu"
+export var year = "1986"
+
+var dress = "pingxiang jiangxi";
+var email = "jzl@jzlzj.com"
+
+export {dress, email}
+
+export function info (name, email){
+	return `my name is ${name}, my email is ${email}`
+}
